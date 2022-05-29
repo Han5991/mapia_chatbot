baf65bccd314d0dcb7e6958a7e856f474806cd82
rewq985 <bnbt3@naver.com>
2022-05-29 23:14:10 +0900

    수정전

--- app.py
+++ app.py
@@ -55,10 +55,10 @@ def chatbot_request(txt_input):
     except InvalidArgument:
         raise
 
-    # print("Query Text : ", response.query_result.query_text)
-    # print("Detected intent : ", response.query_result.intent.display_name)
-    # print("Detected intent confidence : ", response.query_result.intent_detection_confidence)
-    # print("Fulfillment text : ", response.query_result.fulfillment_text)
+    print("Query Text : ", response.query_result.query_text)
+    print("Detected intent : ", response.query_result.intent.display_name)
+    print("Detected intent confidence : ", response.query_result.intent_detection_confidence)
+    print("Fulfillment text : ", response.query_result.fulfillment_text)
 
     return response.query_result.fulfillment_text
 
